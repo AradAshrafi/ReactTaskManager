612289705faaddf966d0eb6f14893d6451b08cae
Arad <Arad.Ashrafi@gmail.com>
2018-02-04 10:34:17 +0330

    header font size updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,26 @@ import { history } from '../routers/AppRouter';
 
 export const Header = props => (
     <div className="header">
-    <div className="content-container">
-    <div className="header__content">
-        <Link className="link header header__title" to="/">Task Manager </Link>
-        {props.isAuth ? (
-            <button className="button">Log Out</button> //handle onClick to LogOut
-        ) : (
-            props.isDashboard && (
-                <div>
-                    <Link className="button button--link" to="/login">Log In</Link>
-                    <Link className="button button--link" to="/signup">Sign Up</Link>
-                </div>
-            )
-        )}
-        </div>
+        <div className="content-container">
+            <div className="header__content">
+                <Link className="link header header__title" to="/">
+                    <h1>Task Manager </h1>
+                </Link>
+                {props.isAuth ? (
+                    <button className="button">Log Out</button> //handle onClick to LogOut
+                ) : (
+                    props.isDashboard && (
+                        <div>
+                            <Link className="button button--link" to="/login">
+                                Log In
+                            </Link>
+                            <Link className="button button--link" to="/signup">
+                                Sign Up
+                            </Link>
+                        </div>
+                    )
+                )}
+            </div>
         </div>
     </div> //handle Style
 );
