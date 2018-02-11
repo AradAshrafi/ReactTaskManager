8d2b12cb835b706fd0e20f0947fb26eb62bf109e
Arad <Arad.Ashrafi@gmail.com>
2018-02-11 16:19:10 +0330

    last commits complete commit :|

---
+++ src/components/PhoneNumModal.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Modal from 'react-modal';
+import { axiosPayment } from '../lib/server';
+
+export default class PhoneNumModal extends React.Component {
+    state = {
+        phoneNum: '',
+    };
+    onPhoneNumChange = (e) => {
+        const phoneNum = e.target.value;
+        this.setState(() => ({
+            phoneNum
+        }));
+    };
+    submitAndMakeModalGo = e => {
+        e.preventDefault();
+        const api="test";
+        const amount=5000;
+        const redirect = "";
+        const mobile = this.state.phoneNum;
+        axiosPayment(api,amount,redirect,mobile);
+    };
+    render() {
+        return (
+            <Modal
+                isOpen={!!this.props.addTaskState }
+                onRequestClose={this.props.closeModal}
+                contentLabel="PhoneNum"
+                closeTimeoutMS={200}
+                className="modal"
+            >
+                <h3 className="modal__title">Phone Number ( For Bank Messages ) : </h3>
+                <input
+                    className="text-input--modal"
+                    placeholder="phoneNum"
+                    type="text"
+                    value={this.state.phoneNum}
+                    onChange={this.onPhoneNumChange}
+                />
+                <button onClick={this.submitAndMakeModalGo} className="button">done</button>
+                <button onClick={this.props.closeModal} className="button">close</button>
+            </Modal>
+        );
+    }
+}
+
